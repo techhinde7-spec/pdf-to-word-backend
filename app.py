f828a1a0d878deb6f6d938d561fc0ca60648e0a6
techhinde7-spec <techhinde7@gmail.com>
2025-08-18 19:17:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,3 +46,8 @@ def convert():
                     break
 
         return send_file(output_path, as_attachment=True)
+        if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
